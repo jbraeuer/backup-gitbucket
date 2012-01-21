1addf85e9c620b6cdd6bba7bcf4276988afe7cf3
Jens Braeuer <jens@numberfour.eu>
2012-01-21 13:14:15 +0100

    Move CLI in own file.

--- lib/backup-gitbucket.rb
+++ lib/backup-gitbucket.rb
@@ -1,22 +1,6 @@
 #! /usr/bin/ruby
-require 'yaml'
 require 'backup-gitbucket/tools.rb'
+require 'backup-gitbucket/cli.rb'
 require 'backup-gitbucket/cloner.rb'
 
-module BackupGitBucket
-    class CLI
-        def initialize(args=ARGV)
-            @args = args
-            @config = YAML::load File.open(@args[0])
-            @global = @config.delete(:global)
-        end
-        def main(args=ARGV)
-            @config.each do |item, opts|
-                klass = Kernel.const_get(item)
-                obj = klass.new(opts.merge(@global))
-                obj.backup
-            end
-        end
-    end
-end
 
---
+++ lib/backup-gitbucket/cli.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+module BackupGitBucket
+    class CLI
+        def initialize(args=ARGV)
+            @args = args
+            @config = YAML::load File.open(@args[0])
+            @global = @config.delete(:global)
+        end
+        def main(args=ARGV)
+            @config.each do |item, opts|
+                klass = Kernel.const_get(item)
+                obj = klass.new(opts.merge(@global))
+                obj.backup
+            end
+        end
+    end
+end
