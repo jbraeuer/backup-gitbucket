5a9c50b2d109462590a8123ab3a4c34efb633334
Jens Braeuer <jens@numberfour.eu>
2012-01-21 14:47:03 +0100

    Add GitHub spec tests.

---
+++ spec/backup-gitbucket/github_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+module BackupGitBucket
+    describe GitHub do
+        describe "#all" do
+
+            let(:opts) do
+                { :username => "testuser",
+                  :password => "testpassword",
+                  :excludes => [ ],
+                  :quiet => true }
+            end
+            let(:repos) do
+                [ { "ssh_url" => "git@github.com:testuser/projectA.git",
+                    "name" => "projectA" },
+                  { "ssh_url" => "git@github.com:testuser/dotfiles.git",
+                    "name" => "dotfiles" } ]
+            end
+            let(:orgs) do
+                [ { "login" => "ACME" } ]
+            end
+            let(:org_repos) do
+                [ { "ssh_url" => "git@github.com:ACME/supersecret.git",
+                    "name" => "supersecret" } ]
+            end
+            before(:each) do
+                Excon.mock = true
+                Excon.stubs.clear
+                Excon.stub({:method => :get, :host => 'api.github.com', :path => "/user/repos"},      {:body => repos.to_json , :status => 200})
+                Excon.stub({:method => :get, :host => 'api.github.com', :path => "/user/orgs"},       {:body => orgs.to_json , :status => 200})
+                Excon.stub({:method => :get, :host => 'api.github.com', :path => "/orgs/ACME/repos"}, {:body => org_repos.to_json , :status => 200})
+            end
+
+            it "should know about personal and organizational repos" do
+                github = BackupGitBucket::GitHub.new opts
+
+                github.all.should have_key("self")
+                github.all.should have_key("ACME")
+            end
+
+            it "lists all my personal repositories" do
+                github = BackupGitBucket::GitHub.new opts
+
+                github.all["self"]["dotfiles"].should == "git@github.com:testuser/dotfiles.git"
+                github.all["self"]["projectA"].should == "git@github.com:testuser/projectA.git"
+            end
+
+            it "lists all my organizational repositories" do
+                github = BackupGitBucket::GitHub.new opts
+
+                github.all["ACME"]["supersecret"].should == "git@github.com:ACME/supersecret.git"
+            end
+
+            it "excludes matching projects" do
+                opts[:excludes] = [ "oject", "secret" ]
+
+                github = BackupGitBucket::GitHub.new opts
+
+                github.all["self"]["dotfiles"].should == "git@github.com:testuser/dotfiles.git"
+                github.all["self"].should_not have_key("projectA")
+                github.all["ACME"].should_not have_key("supersecret")
+            end
+
+            it "excludes matching organizations" do
+                opts[:excludes] = [ "oject", "ACME" ]
+
+                github = BackupGitBucket::GitHub.new opts
+                github.all.should_not have_key("ACME")
+                github.all.should have_key("self")
+            end
+        end
+    end
+end
